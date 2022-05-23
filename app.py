719369a7d63a194c82471192921c01538e225f3e
yourusername <eduardoazevedo08@gmail.com>
2022-05-23 20:52:06 -0300

    criação de rota about

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "No pain no gain"
 
+@app.route("/about")
+def about():
+    return "<h1 style='color: red'>About!!!!</h1>"
+
 if __name__ == "__main__":
     app.run()
