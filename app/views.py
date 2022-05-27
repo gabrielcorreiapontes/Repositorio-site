7d20b38de7ce609b8d5d74e222e77b583463df47
yourusername <eduardoazevedo08@gmail.com>
2022-05-27 18:00:49 -0300

    Criação de index html, edição do file views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from app import app
 
+from flask import render_template
+
 @app.route("/")
 def index():
-    return "No pain no gain"
+    return render_template("index.html")
     
 @app.route("/about")
 def about():
