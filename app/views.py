260ece2afe49866373d2619265d603dd9e1840ae
yourusername <eduardoazevedo08@gmail.com>
2022-05-27 18:28:50 -0300

    Criação de template publico e admin

--- app/views.py
+++ app/views.py
@@ -4,8 +4,20 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("/public/index.html")
+
+@app.route("/product")
+def products():
+    return "<h1 style='color: red'>produtogtx2</h1>"
+
+@app.route("/categories")
+def categories():
+    return "<h1 style='color: red'>categorias</h1>"
+
+@app.route("/user")
+def users():
+    return "<h1 style='color: red'>users</h1>"
     
 @app.route("/about")
 def about():
-    return "<h1 style='color: red'>About!!!!</h1>"
+    return "<h1 style='color: red'>SPP - Sell PC Parts</h1>"
